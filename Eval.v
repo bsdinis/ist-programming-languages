14a9143bdb72e4752afd3c41921464a3cf1166bc
bsdinis <baltasar.dinis@tecnico.ulisboa.pt>
2021-04-26 16:30:33 +0100

    no good commit msg for you

--- Eval.v
+++ Eval.v
@@ -84,24 +84,69 @@ Fixpoint ceval_step (st : state) (c : com) (i : nat)
 
 (* (ceval_step st_i c2 i) C (ceval_step st c1;c2 (S i)) *)
 
-Lemma middle_state_exists: forall c1 c2 st st' i,
-  In (Some st') (ceval_step st <{c1 ; c2}> (S i)) ->
+Lemma seq_middle_state_exists: forall c1 c2 st st' i,
+  In (Some st') (ceval_step st <{c1 ; c2}> (S i)) <->
     (exists st_i, In (Some st_i) (ceval_step st c1 i) /\ In (Some st') (ceval_step st_i c2 i) ).
 Proof.
-  intros c1 c2 st st' i H.
-  simpl in H.
-  apply in_flat_map in H.
-  destruct H.
-  destruct H as [H_x_in].
-  destruct x.
-  - eexists.
-    split.
-    -- apply H_x_in.
-    -- apply H.
-  - simpl in H.
+  intros c1 c2 st st' i.
+  split.
+  - intros H.
+    simpl in H.
+    apply in_flat_map in H.
     destruct H.
-    -- inversion H.
-    -- contradiction.
+    destruct H as [H_x_in].
+    destruct x.
+    -- eexists.
+      split.
+      --- apply H_x_in.
+      --- apply H.
+    -- simpl in H.
+        destruct H.
+      --- inversion H.
+      --- contradiction.
+  - intros H.
+    destruct H as [st_i H].
+    destruct H as [H1 H2].
+    simpl.
+    apply in_flat_map.
+    eexists.
+    split.
+    -- apply H1.
+    -- apply H2.
+Qed.
+
+Lemma while_middle_state_exists: forall b c st st' i,
+  beval st b = true ->
+  (In (Some st') (ceval_step st <{ while b do c end}> (S i)) <->
+    (exists st_i, In (Some st_i) (ceval_step st c i) /\ In (Some st') (ceval_step st_i <{ while b do c end}> i) )).
+Proof.
+  intros b c st st' i Hb.
+  split.
+  - intros H.
+    simpl in H.
+    rewrite Hb in H.
+    apply in_flat_map in H.
+    destruct H.
+    destruct H as [H_x_in].
+    destruct x.
+    -- eexists.
+      split.
+      --- apply H_x_in.
+      --- apply H.
+    -- simpl in H.
+      destruct H.
+      --- inversion H.
+      --- contradiction.
+  - intros H.
+    destruct H as [st_i H].
+    destruct H as [H1 H2].
+    simpl.
+    rewrite Hb.
+    apply in_flat_map.
+    eexists.
+    split.
+    -- apply H1.
+    -- apply H2.
 Qed.
 
 (**
@@ -140,7 +185,7 @@ Proof.
        --- contradiction.
 
     -- (* ; *)
-      apply middle_state_exists in H.
+      apply seq_middle_state_exists in H.
       destruct H.
       destruct H as [H1 H2].
       apply Hi' in H1.
@@ -148,44 +193,39 @@ Proof.
       apply E_Seq with x; assumption.
     -- (* !! *)
        apply in_app_or in H. apply E_NonDet. destruct H.
-       + (* left *) left. apply Hi'. assumption.
-       + (* right *) right. apply Hi'. assumption.
+       --- (* left *) left. apply Hi'. assumption.
+       --- (* right *) right. apply Hi'. assumption.
 
     -- (* if *)
        destruct (beval st b) eqn:Heqr.
-       + (* r = true *)
+       --- (* r = true *)
          apply E_IfTrue. rewrite Heqr. reflexivity.
          unfold ceval_step in H.
          rewrite Heqr in H.
          apply Hi'. assumption.
-       + (* r = false *)
+       --- (* r = false *)
          apply E_IfFalse. rewrite Heqr. reflexivity.
          unfold ceval_step in H.
          rewrite Heqr in H.
          apply Hi'. assumption.
 
     -- (* while *)
-      destruct (beval st b) eqn :Heqr.
+      destruct (beval st b) eqn :Hguard.
       --- (* r = true *)
-        simpl in H.
-        rewrite Heqr in H.
-        apply in_flat_map in H.
-        destruct H as [mid_opt_state].
-        destruct H as [H_middle].
-        apply Hi'.
-        destruct (ceval_step st' c i') eqn:Heqr1.
-        + (* [] *) admit.
-        +
-          admit.
+          apply while_middle_state_exists in H; try assumption.
+          destruct H.
+          destruct H as [H1 H2].
+          apply Hi' in H1.
+          apply Hi' in H2.
+          apply E_WhileTrue with x; assumption.
       --- (* r = false *)
         simpl in H.
-        rewrite Heqr in H.
+        rewrite Hguard in H.
         simpl in H.
         destruct H.
-          + injection H as H2. rewrite <- H2. apply E_WhileFalse. apply Heqr.
-          + contradiction.
-
-Admitted.
+        ---- injection H as H2. rewrite <- H2. apply E_WhileFalse. apply Hguard.
+        ---- contradiction.
+Qed.
 
 Theorem ceval_step_more: forall i1 i2 st st' c,
   i1 <= i2 ->
@@ -204,24 +244,46 @@ Proof.
     -- (* := *)
        simpl in Hceval. simpl. assumption.
     -- (* ; *)
-      admit.
-      (* simpl in Hceval. simpl. *)
-      (* destruct (ceval_step st c1 i1') eqn:Heqst1'o. *)
-      (* --- contradiction. *)
-      (* --- destruct (ceval_step st c1 i2'). *)
-      (*   + admit. *)
-      (*   + admit. *)
+       simpl in Hceval.
+       apply seq_middle_state_exists in Hceval.
+       apply seq_middle_state_exists.
+       destruct Hceval as [x Hceval].
+       destruct Hceval as [H1 H2].
+       apply (IHi1' (i2') _ _ _ ) in H1 ; try lia.
+       apply (IHi1' (i2') _ _ _ ) in H2 ; try lia.
+       eexists.
+       split.
+       --- apply H1.
+       --- apply H2.
     -- (* !! *)
         simpl in Hceval. simpl. apply in_app_or in Hceval. apply in_app_iff.
         destruct Hceval.
-        + left. apply IHi1'; assumption.
-        + right. apply IHi1'; assumption.
+        --- left. apply IHi1'; assumption.
+        --- right. apply IHi1'; assumption.
     -- (* if *)
        simpl in Hceval. simpl.
        destruct (beval st b) ; apply IHi1'; assumption.
     -- (* while *)
-       admit.
-Admitted.
+       (* simpl in Hceval *)
+       destruct (beval st b) eqn:Hguard.
+       --- (* guard is true *)
+           apply while_middle_state_exists in Hceval; try assumption.
+           apply while_middle_state_exists; try assumption.
+           destruct Hceval as [st_i Hceval].
+           destruct Hceval as [H1 H2].
+           apply (IHi1' (i2') _ _ _ ) in H1 ; try lia.
+           apply (IHi1' (i2') _ _ _ ) in H2 ; try lia.
+           eexists.
+           split.
+           ---- apply H1.
+           ---- apply H2.
+       --- (* guard is false *)
+           simpl in Hceval.
+           simpl.
+           rewrite Hguard in Hceval.
+           rewrite Hguard.
+           assumption.
+Qed.
 
 (* ceval_step_more can be used in the proof of ceval__ceval_step *)
 Theorem ceval__ceval_step: forall c st st',
@@ -232,29 +294,35 @@ Proof.
     induction Hce.
     - (* skip *)
       assert (In (Some st) (ceval_step st <{ skip }> 1)).
-      + simpl. left. reflexivity.
-      + eexists. apply H.
+      -- simpl. left. reflexivity.
+      -- eexists. apply H.
     - (* := *)
-      assert (In (Some (x !-> n; st)) (ceval_step st <{ x := a }> 1)).
-      + simpl. left. rewrite H. reflexivity.
-      + eexists. apply H0.
+      assert (In (Some (x !-> n; st)) (ceval_step st <{ x := a }> 1)) as Hin.
+      -- simpl. left. rewrite H. reflexivity.
+      -- eexists. apply Hin.
     - (* ; *)
-      destruct IHHce1 as [i1 H1]. destruct IHHce2 as [i2 H2].
-      admit.
+      destruct IHHce1 as [i1 IHc1].
+      destruct IHHce2 as [i2 IHc2].
+      assert ((ceval_step st' c2 i2) = (ceval_step st <{ c1; c2 }> (1 + i1 + i2))) as Heq.
+      -- simpl. apply (ceval_step_more (i1) (i1+i2) _ _ c1).
+      -- eexists. rewrite <- Heq. assumption.
     - (* !! *)
-      destruct H.
-      + (* st =[ c1 ]=> st' *)
-        unfold ceval_step.
-        simpl.
-        admit.
-      + (* st =[ c2 ]=> st' *)
-        admit.
+      apply E_NonDet in H.
+      admit.
     - (* if true *)
-      admit.
+      destruct IHHce as [i IHc1].
+      assert ((ceval_step st c1 i) = (ceval_step st <{ if b then c1 else c2 end }> (S i))).
+      -- simpl. rewrite H. reflexivity.
+      -- eexists. rewrite <- H0. assumption.
     - (* if false *)
-      admit.
+      destruct IHHce as [i IHc2].
+      assert ((ceval_step st c2 i) = (ceval_step st <{ if b then c1 else c2 end }> (S i))) as Heq.
+      -- simpl. rewrite H. reflexivity.
+      -- eexists. rewrite <- Heq. assumption.
     - (* while false *)
-      admit.
+      assert ((ceval_step st <{skip}> 1) = (ceval_step st <{ while b do c end }> 2)) as Heq.
+      -- simpl. rewrite H. reflexivity.
+      -- eexists. rewrite <- Heq. simpl. left. reflexivity.
     - (* while true *)
       admit.
 Admitted.
